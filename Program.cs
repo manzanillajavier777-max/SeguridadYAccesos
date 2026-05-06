8576dc85d8553ba4e81a1187ad566b246810e8d1
manzanillajavier777-max <manzanillajavier777@gmail.com>
2026-05-05 22:25:07 -0400

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -4,8 +4,8 @@ using Microsoft.EntityFrameworkCore;
 var builder = WebApplication.CreateBuilder(args);
 
 // Obtener connection string desde appsettings o variables de entorno (Render)
-var connectionString = builder.Configuration.GetConnectionString("Connection") 
-    ?? Environment.GetEnvironmentVariable("ConnectionStrings__ConnectionSeguridadyAccesos");
+var connectionString = builder.Configuration.GetConnectionString("ConnectionStrings__Connection") 
+    ?? Environment.GetEnvironmentVariable("Connection");
 
 // Configuración de DbContext con PostgreSQL
 builder.Services.AddDbContext<AppDbContext>(options =>
